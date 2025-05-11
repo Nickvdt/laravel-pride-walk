f16e3918b47e55b8573b046c0f06586ab17b347a
NickvanderTol <njm.vandertol@gmail.com>
2025-05-11 21:49:40 +0200

    Seeder Timestap fix

--- database/seeders/ExhibitionScheduleSeeder.php
+++ database/seeders/ExhibitionScheduleSeeder.php
@@ -15,9 +15,9 @@ class ExhibitionScheduleSeeder extends Seeder
 
         $exhibitions = [
             ['title' => 'PEEKABOO', 'start' => '12:00', 'end' => '18:00'], // 1
-            ['title' => 'WALLPAPER', 'start' => '13:00', 'end' => '19:00', 'daily' => true], // 3
-            ['title' => 'Dans volk', 'start' => '09:00', 'end' => '21:00', 'daily' => true], // 4
-            ['title' => 'MY FETISH EYE', 'start' => '11:00', 'end' => '19:00', 'daily' => true], // 5
+            ['title' => 'WALLPAPER', 'start' => '13:00', 'end' => '19:00'], // 3
+            ['title' => 'Dans volk', 'start' => '09:00', 'end' => '21:00'], // 4
+            ['title' => 'MY FETISH EYE', 'start' => '11:00', 'end' => '19:00'], // 5
             ['title' => 'Roze Reuzen', 'weekdays' => [ // 8
                 'zondag' => ['10:00', '20:00'],
                 'maandag' => ['08:00', '22:00'],
@@ -27,32 +27,34 @@ class ExhibitionScheduleSeeder extends Seeder
                 'vrijdag' => ['08:00', '22:00'],
                 'zaterdag' => ['10:00', '20:00']
             ]],
-            ['title' => 'MOREPIXX', 'start' => '17:00', 'end' => '19:00', 'start_date' => '2025-07-19', 'end_date' => '2025-08-03', 'daily' => true], // 9
+            ['title' => 'MOREPIXX', 'start' => '17:00', 'end' => '19:00', 'start_date' => '2025-07-19', 'end_date' => '2025-08-03'], // 9
             ['title' => 'KUNSTRUIM', 'start' => '12:00', 'end' => '18:00', 'start_date' => '2025-07-09', 'end_date' => '2025-08-03',], // 10
-            ['title' => '*DIED*', 'start' => '12:00', 'end' => '18:00', 'start_date' => '2025-07-31', 'end_date' => '2025-08-03'],// 11
+            ['title' => '*DIED*', 'start' => '12:00', 'end' => '18:00', 'start_date' => '2025-07-31', 'end_date' => '2025-08-03'], // 11
             ['title' => 'MAMABIRD', 'start' => '12:00', 'end' => '18:00'], // 12
-            ['title' => 'MARTIN AT FREE WILLY', 'start' => '20:00', 'end' => '23:59', 'daily' => true], // 13
-            ['title' => 'Gemma Leys en Bastiënne Kramer', 'start' => '12:00', 'end' => '18:00', 'daily' => true], // 14
-            ['title' => 'Prins de Vos', 'start' => '11:00', 'end' => '23:00', 'start_date' => '2025-07-18', 'end_date' => '2025-08-24', 'daily' => true], // 17
-            ['title' => 'TIM&RISK&DEVON', 'start' => '09:00', 'end' => '21:00', 'daily' => true], // 18
-            ['title' => 'MEN BY NIELS', 'start' => '09:00', 'end' => '21:00', 'daily' => true], // 19
-            ['title' => 'Pedro Matias', 'start' => '09:00', 'end' => '21:00', 'daily' => true], // 22
+            ['title' => 'MARTIN AT FREE WILLY', 'start' => '20:00', 'end' => '23:59'], // 13
+            ['title' => 'Gemma Leys en Bastiënne Kramer', 'start' => '12:00', 'end' => '18:00'], // 14
+            ['title' => 'Prins de Vos', 'start' => '11:00', 'end' => '23:00', 'start_date' => '2025-07-18', 'end_date' => '2025-08-24'], // 17
+            ['title' => 'TIM&RISK&DEVON', 'start' => '09:00', 'end' => '21:00'], // 18
+            ['title' => 'MEN BY NIELS', 'start' => '09:00', 'end' => '21:00'], // 19
+            ['title' => 'Pedro Matias', 'start' => '09:00', 'end' => '21:00'], // 22
 
         ];
 
         foreach ($exhibitions as $exhibition) {
             $model = Exhibition::where('title', $exhibition['title'])->first();
             if ($model) {
-                $startDate = isset($exhibition['start_date']) ? Carbon::parse($exhibition['start_date']) : $defaultStartDate;
-                $endDate = isset($exhibition['end_date']) ? Carbon::parse($exhibition['end_date']) : $defaultEndDate;
+                $startDate = isset($exhibition['start_date']) ? Carbon::parse($exhibition['start_date'])->toDateString() : $defaultStartDate->toDateString();
+                $endDate = isset($exhibition['end_date']) ? Carbon::parse($exhibition['end_date'])->toDateString() : $defaultEndDate->toDateString();
 
-                for ($date = $startDate->copy(); $date->lte($endDate); $date->addDay()) {
+                for ($date = Carbon::parse($startDate); $date->lte(Carbon::parse($endDate)); $date->addDay()) {
                     $dayOfWeek = strtolower($date->format('l'));
                     $start = $exhibition['start'] ?? '00:00';
                     $end = $exhibition['end'] ?? '00:00';
+
                     if (isset($exhibition['weekdays']) && isset($exhibition['weekdays'][$dayOfWeek])) {
                         list($start, $end) = $exhibition['weekdays'][$dayOfWeek];
                     }
+
                     $model->schedules()->create([
                         'date' => $date->toDateString(),
                         'start_time' => $start,
