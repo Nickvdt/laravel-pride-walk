4b7f09f1f81003a8653b11761c944b4ac831bbc0
Nickvdt <njm.vandertol@gmail.com>
2025-06-02 09:41:10 +0200

    production ready

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,15 +5,23 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
     public function run()
     {
+
+        $plainPassword = Str::random(16);
+
         User::create([
             'name' => 'admin',
             'email' => 'admin@example.com',
-            'password' => Hash::make('admin123'),
+            'password' => Hash::make($plainPassword),
         ]);
+
+        echo "\nAdmin login:\n";
+        echo "Email: admin@example.com\n";
+        echo "Password: {$plainPassword}\n";
     }
 }
